065e78c4b868b1a8b5e35c41f0fb1d2ffc1c3cb4
Rienzi Gokea <RGokea@gmail.com>
2022-01-16 13:33:37 -0500

    Broke out all funcs

--- server/request_helpers.go
+++ server/request_helpers.go
@@ -15,42 +15,64 @@ type InputObject interface {
 	Validate() error
 }
 
-func PreProcessInput(input InputObject, w http.ResponseWriter, r *http.Request, maxBytes int) (error, int) {
-	var err error
+/*********************************************
+ * Preprocess + Read Inputs
+ * *******************************************/
 
-	max := 131072
+func PreProcessInput(input InputObject, maxBytes int, w http.ResponseWriter, r *http.Request, unmarshalFn func(interface{}, *http.Request) error) error {
+
+	max := 524288
 	if maxBytes != 0 {
 		max = maxBytes
 	}
 	// Block the read of any body too large in order to help prevent DoS attacks
 	r.Body = http.MaxBytesReader(w, r.Body, int64(max))
+	if err := unmarshalFn(input, r); err != nil {
+		return err
+	}
 
+	return input.Validate()
+}
+
+func PreProcessInputFromHeaders(input InputObject, maxBytes int, w http.ResponseWriter, r *http.Request) error {
+	return PreProcessInput(input, maxBytes, w, r, UnmarshalObjectFromHeaders)
+}
+
+func PreProcessInputFromJSON(input InputObject, maxBytes int, w http.ResponseWriter, r *http.Request) error {
+	return PreProcessInput(input, maxBytes, w, r, UnmarshalObjectFromJSONStrict)
+}
+
+func PreProcessInputFromJSONAPI(input InputObject, maxBytes int, w http.ResponseWriter, r *http.Request) error {
+	return PreProcessInput(input, maxBytes, w, r, func(interface{}, *http.Request) error {
+		return jsonapi.UnmarshalPayload(r.Body, &input)
+	})
+}
+
+func UnmarshalObjectFromHeaders(input interface{}, r *http.Request) error {
 	header := r.Header.Get(ContentTypeHeader)
 	if header == JSONContentType {
-
-		dec := json.NewDecoder(r.Body)
-		dec.DisallowUnknownFields()
-
-		if err = dec.Decode(input); err != nil {
-			return err, http.StatusBadRequest
-		}
-
+		return UnmarshalObjectFromJSONStrict(input, r)
 	} else if header == jsonapi.MediaType {
-
-		if err := jsonapi.UnmarshalPayload(r.Body, &input); err != nil {
-			return err, http.StatusBadRequest
-		}
-
+		return jsonapi.UnmarshalPayload(r.Body, &input)
 	} else {
-		return errors.New("Content-Type header is not json or jsonapi standard"), http.StatusBadRequest
+		return errors.New("Content-Type header is not json or jsonapi standard")
 	}
-
-	if err = input.Validate(); err != nil {
-		return err, http.StatusBadRequest
-	}
-
-	return nil, 0
 }
+
+func UnmarshalObjectFromJSON(input interface{}, r *http.Request) error {
+	dec := json.NewDecoder(r.Body)
+	return dec.Decode(input)
+}
+
+func UnmarshalObjectFromJSONStrict(input interface{}, r *http.Request) error {
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	return dec.Decode(input)
+}
+
+/*********************************************
+ * Error Handling
+ * *******************************************/
 
 func SendErrorOnError(err error, status int, w http.ResponseWriter, r *http.Request, logError func(error, *http.Request)) {
 	if err != nil {
@@ -59,25 +81,29 @@ func SendErrorOnError(err error, status int, w http.ResponseWriter, r *http.Requ
 	}
 }
 
-func WriteJSONModelToResponse(dataToSend interface{}, status int, w http.ResponseWriter) error {
+/*********************************************
+ * Writing Outputs
+ * *******************************************/
+
+func WriteModelToResponseJSON(dataToSend interface{}, status int, w http.ResponseWriter) error {
 	w.Header().Set(ContentTypeHeader, JSONContentType)
 	// NOTE: w.WriteHeader must go after all other header writes but before json encoder or else it/headers will not be picked up
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(dataToSend)
 }
 
-func WriteJSONAPIModelToResponse(dataToSend interface{}, status int, w http.ResponseWriter) error {
+func WriteModelToResponseJSONAPI(dataToSend interface{}, status int, w http.ResponseWriter) error {
 	w.Header().Set(ContentTypeHeader, jsonapi.MediaType)
 	// NOTE: w.WriteHeader must go after all other header writes but before json encoder or else it/headers will not be picked up
 	w.WriteHeader(status)
 	return jsonapi.MarshalPayload(w, dataToSend)
 }
 
-func WriteAgnosticModelToResponse(dataToSend interface{}, status int, w http.ResponseWriter, r *http.Request) error {
+func WriteModelToResponseFromHeaders(dataToSend interface{}, status int, w http.ResponseWriter, r *http.Request) error {
 	header := r.Header.Get(ContentTypeHeader)
 	acceptHeader := r.Header.Get(AcceptContentTypeHeader)
 	if header == jsonapi.MediaType || acceptHeader == jsonapi.MediaType {
-		return WriteJSONAPIModelToResponse(dataToSend, status, w)
+		return WriteModelToResponseJSONAPI(dataToSend, status, w)
 	}
-	return WriteJSONModelToResponse(dataToSend, status, w)
+	return WriteModelToResponseJSON(dataToSend, status, w)
 }
